8e2920c4a12fba330a0f28c3e564117c811fd925
She7eto <113710619+mohamed-sheheto@users.noreply.github.com>
2026-08-03 18:23:04 +0300

    Add: remove-duplicates-from-sorted-array - submission-4

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-4.js
@@ -0,0 +1,13 @@
+class Solution {
+  removeDuplicates(nums) {
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] === nums[j]) {
+          nums.splice(j, 1);
+          i = -1;
+        }
+      }
+    }
+    return nums.length;
+  }
+}
