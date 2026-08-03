3e6db38bb4e10fc4d33255abb79d6acbc4cc5973
She7eto <113710619+mohamed-sheheto@users.noreply.github.com>
2026-08-03 12:19:50 +0300

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        auto it = lower_bound(nums.begin(), nums.end(), target);
+        if (it != nums.end() && *it == target) {
+            return distance(nums.begin(), it);
+        }
+        return -1;
+    }
+};
